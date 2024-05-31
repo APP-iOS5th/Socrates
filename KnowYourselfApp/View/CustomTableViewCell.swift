2f41ce53b7ca064c0f9a533d4a1014307b56b672
Hyungjun Kim <hjhj919@naver.com>
2024-05-31 21:53:23 +0900

    mainView

--- KnowYourselfApp/View/CustomTableViewCell.swift
+++ KnowYourselfApp/View/CustomTableViewCell.swift
@@ -7,17 +7,57 @@
 
 import UIKit
 
+protocol CustomCellDelegate: AnyObject {
+    func CustomCellButtonDidTapped(_ cell: CustomTableViewCell, isTapped: Bool)
+}
+
 class CustomTableViewCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    static let identifier = "CustomCell"
+    
+     let cellImageView: UIImageView = {
+       let imageview = UIImageView()
+        imageview.image = UIImage(named: "aaa")
+        imageview.contentMode = .scaleAspectFill
+        imageview.layer.masksToBounds = true
+        imageview.layer.cornerRadius = 30
+        return imageview
+    }()
+    
+      let entryButton: UIButton = {
+       let button = UIButton()
+        button.setTitle("입장", for: .normal)
+        button.backgroundColor = .black
+        button.layer.cornerRadius = 20
+        button.isHidden = true
+        return button
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super .init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        contentView.addSubview(cellImageView)
+        contentView.addSubview(entryButton)
+        cellImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        entryButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        cellImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 0).isActive = true
+        
+        cellImageView.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 1.0).isActive = true
+        
+        NSLayoutConstraint.activate([
+            cellImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            cellImageView.heightAnchor.constraint(equalTo: contentView.heightAnchor, multiplier: 0.93),
+            entryButton.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -20),
+            entryButton.heightAnchor.constraint(equalTo: contentView.heightAnchor, multiplier: 0.3),
+            entryButton.widthAnchor.constraint(equalToConstant: 50),
+            entryButton.centerYAnchor.constraint(equalTo: contentView.centerYAnchor, constant: -7)
+        ])
+        
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
-
 }
--- KnowYourselfApp/View/MainViewController.swift
+++ KnowYourselfApp/View/MainViewController.swift
@@ -8,23 +8,47 @@
 import UIKit
 
 class MainViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    
-    
    
     let tableView = UITableView()
+    var selectedCell: CustomTableViewCell?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        self.title = "KNOW YOURSELF"
+        tableView.frame = view.bounds
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(CustomTableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.rowHeight = view.frame.size.height / 4.7
+        tableView.estimatedRowHeight = 100
+        tableView.separatorStyle = .none
         view.addSubview(tableView)
+        
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return CustomTableViewCell()
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? CustomTableViewCell else {
+                return UITableViewCell()
+            }
+        cell.entryButton.isHidden = true
+        return cell
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 4
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        guard let cell = tableView.cellForRow(at: indexPath) as? CustomTableViewCell else { return }
+        if let preCell = selectedCell {
+            preCell.entryButton.isHidden = true
+            preCell.cellImageView.image = UIImage(named: "aaa")
+        }
+        cell.entryButton.isHidden = false
+        cell.cellImageView.image = UIImage(named: "bbb")
+        selectedCell = cell
     }
 
     

2e924050e0a5af54163da74d64bb64b4095f05c1
sohee <danwater86@gmail.com>
2024-06-10 09:49:07 +0900

    add recommendedMenu

--- KnowYourselfApp/DinnerGenie/DinnerGenieTestViewController.swift
+++ KnowYourselfApp/DinnerGenie/DinnerGenieTestViewController.swift
@@ -164,13 +164,13 @@ class FourthTestViewController: UIViewController {
         }()
         private let resultImageView: UIImageView = {
             let imageView = UIImageView()
-            imageView.contentMode = .scaleAspectFit
+            imageView.contentMode = .scaleAspectFill
+            imageView.clipsToBounds = true
             imageView.translatesAutoresizingMaskIntoConstraints = false
             return imageView
         }()
         private let resultLabel: UILabel = {
             let label = UILabel()
-            label.text = "추천메뉴: 나가사키짬뽕"
             label.font = UIFont.systemFont(ofSize: 20)
             label.textAlignment = .center
             label.translatesAutoresizingMaskIntoConstraints = false
@@ -225,6 +225,8 @@ class FourthTestViewController: UIViewController {
             view.addSubview(saveButton)
             view.addSubview(homeButton)
             setupConstraints()
+            updateResult()
+            
         }
         
         private func setupConstraints() {
@@ -269,6 +271,38 @@ class FourthTestViewController: UIViewController {
         
         private func updateResult() {
             
+            let recommendedMenu = getRecommendedMenu()
+            resultImageView.image = UIImage(named: recommendedMenu)
+            resultLabel.text = "추천 메뉴: \(recommendedMenu)"
+            
+        func getRecommendedMenu() -> String {
+            let drinkOptions = ["치킨", "짬뽕", "마라탕", "피자", "회/초밥", "족발/보쌈", "김치찌개", "삼겹살", "떡볶이", "팔보채", "곱창/막창", "파전", "양꼬치", "닭발", "해물탕", "감자탕", "홍합탕", "나가사끼짬뽕", "제육볶음", "아구찜", "불닭볶음면", "라면", "쭈꾸미볶음", "부대찌개", "국밥", "설렁탕", "알탕", "순대볶음", "매운족발"]
+            let nonDrinkOptions = ["치킨", "짜장면", "짬뽕", "햄버거", "마라탕", "피자", "회/초밥", "족발/보쌈",  "김치찌개", "된장찌개", "불고기", "비빔밥", "삼겹살", "갈비찜", "떡볶이", "팔보채", "곱창/막창", "파스타", "양꼬치", "닭발", "해물탕", "감자탕", "홍합탕", "나가사끼짬뽕", "제육볶음", "아구찜", "순두부찌개", "김밥", "김치볶음밥", "냉면", "불닭볶음면", "라면", "돈가스", "쭈꾸미볶음", "부대찌개", "국밥", "설렁탕", "알탕", "순대볶음", "매운족발", "쌀국수"]
+            var options: [String]
+            
+            if isDrinking {
+                if wantsSpicy && wantsSoup {
+                    options = ["마라탕", "김치찌개", "해물탕", "부대찌개", "알탕", "감자탕", "라면", "짬뽕"]
+                } else if wantsSpicy && !wantsSoup {
+                    options = ["닭발", "떡볶이", "제육볶음", "아구찜", "불닭볶음면", "쭈꾸미볶음", "양꼬치", "순대볶음", "매운족발"]
+                } else if !wantsSpicy && wantsSoup {
+                    options = ["홍합탕", "나가사끼짬뽕", "국밥", "설렁탕"]
+                } else {
+                    options = ["치킨", "피자", "회/초밥", "족발/보쌈", "삼겹살", "팔보채", "곱창/막창", "파전"]
+                }
+            } else {
+                if wantsSpicy && wantsSoup {
+                    options = ["마라탕", "김치찌개", "해물탕", "부대찌개", "알탕", "감자탕", "라면", "짬뽕"]
+                } else if wantsSpicy && !wantsSoup {
+                    options = ["닭발", "떡볶이", "제육볶음", "아구찜", "불닭볶음면", "쭈꾸미볶음", "양꼬치", "순대볶음", "김치볶음밥", "매운족발"]
+                } else if !wantsSpicy && wantsSoup {
+                    options = ["홍합탕", "나가사끼짬뽕", "국밥", "설렁탕", "된장찌개", "냉면", "쌀국수"]
+                } else {
+                    options = ["치킨", "피자", "회/초밥", "족발/보쌈", "삼겹살", "팔보채", "곱창/막창",  "짜장면", "햄버거", "불고기", "비빔밥", "갈비찜", "파스타", "김밥", "돈가스"]
+                }
+            }
+                
+                return options.randomElement() ?? "추천할 메뉴가 없습니다."
+            }
         }
     }
-
